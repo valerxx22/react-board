8f11b625ab55fc735d8ad8e74c6ac335192b0ad3
Valeriu Crudu <valeriu.crudu@sunrise.net>
2018-10-04 18:10:08 +0300

    [new] Adding react layout, services and components (home, profile, user, issues)

--- src/main/jsx/app.jsx
+++ src/main/jsx/app.jsx
@@ -5,6 +5,18 @@ import ReactDOM from 'react-dom';
 import {Router, Route, IndexRoute, hashHistory} from 'react-router';
 import MainLayout from './componenets/layout/main-layout';
 import Home from './componenets/home';
+import OneColumnLayout from './componenets/layout/one-column-layout';
+import LeftSideColumnLayout from './componenets/layout/left-side-column-layout'
+import ProjectList from './componenets/project-list';
+import ProjectForm from './componenets/project-form';
+import IssueList from './componenets/issue-list';
+import IssueForm from './componenets/issue-form';
+import IssueView from './componenets/issue-view';
+import UserList from './componenets/user-list';
+import UserForm from './componenets/user-form';
+import Profile from './componenets/profile';
+import Settings from './componenets/settings';
+import UserSidebar from "./componenets/user-sidebar";
 
 class App extends Component {
     render() {
@@ -12,6 +24,28 @@ class App extends Component {
             <Router history={hashHistory}>
                 <Route path="/" component={MainLayout}>
                     <IndexRoute component={Home}/>
+                    <Route path="projects/:projectKey/issues" component={OneColumnLayout}>
+                        <IndexRoute component={IssueList}/>
+                    </Route>
+                    <Route path="issues" component={OneColumnLayout}>
+                        <Route path=":projectKey/create" component={IssueForm}/>
+                        <Route path=":issueId/edit" component={IssueForm}/>
+                        <Route path=":issueId" component={IssueView}/>
+                    </Route>
+                    <Route path="profile" component={Profile}/>
+                    <Route path="settings" component={Settings}/>
+                    <Route path="admin" component={LeftSideColumnLayout}>
+                        <Route path="projects">
+                            <IndexRoute components={{main:ProjectList}}/>
+                            <Route path="create" components={{main:ProjectForm}}/>
+                            <Route path="edit/:projectId" components={{main:ProjectForm}}/>
+                        </Route>
+                        <Route path="users">
+                            <IndexRoute components={{main:UserList, sidebar: UserSidebar}}/>
+                            <Route path="create" components={{main:UserForm, sidebar: UserSidebar}}/>
+                            <Route path="edit/:userName" components={{main:UserForm, sidebar: UserSidebar}}/>
+                        </Route>
+                    </Route>
                 </Route>
             </Router>
         )
---
+++ src/main/jsx/components/settings.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Settings extends Component {
+    render() {
+        return (
+            <div className="col-md-12">
+                <h3>Settings Page</h3>
+            </div>
+        )
+    }
+}
+
+export default Settings;
